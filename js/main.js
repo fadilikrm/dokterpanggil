f4749964f89dc5950a6aefe432c296f945e8ac5e
fadilikrm <fadilikram087@gmail.com>
2023-11-10 20:20:43 +0800

    done

--- js/main.js
+++ js/main.js
@@ -59,6 +59,14 @@
             }
         }
     });
+
+    $(".service a").on('click', function (event) {
+        if (this.hash !== "") {
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top - 35
+            }, 1000, 'easeInOutExpo');
+        }
+    });
     
     
     // Typed Initiate
